422c4dc7a53f4b505065398c5161e12f1e8fd533
bobo <syswjb@126.com>
2021-01-01 21:02:53 +0800

    初始版本1.0

---
+++ git-test-ideaProject/src/org/ccunix/FirstClass.java
@@ -0,0 +1,18 @@
+package org.ccunix;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("真的好想你，我是第1个版本！！！");
+        System.out.println("真的好想你，我是第1个版本！！！");
+        System.out.println("真的好想你，我是第1个版本！！！");
+        System.out.println("真的好想你，我是第1个版本！！！");
+        System.out.println("真的好想你，我是第1个版本！！！");
+        System.out.println("真的好想你，我是第1个版本！！！");
+    }
+    public void show(){
+        System.out.println("show1111");
+        System.out.println("show1111");
+        System.out.println("show1111");
+        System.out.println("show1111");
+    }
+}
